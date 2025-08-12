72f1373348e642434f6894c82b6107260aa53af4
吳元皓 <hw@yuanhau.com>
2025-08-12 12:00:06 +0800

    Making a basic cli tool

--- cli_app/index.ts
+++ cli_app/index.ts
@@ -1,15 +1,24 @@
 import { generateRandomString } from "./app/functions";
 import fs from "fs";
+import path from "path";
+import os from "os";
 
-const filepath = "/home/howard/.authly/computerId";
+const filepath = path.join(os.homedir(), ".authly", "computerId");
+const dirPath = path.dirname(filepath);
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath, { recursive: true });
+}
 const server = process.env.SERVER_URL;
 async function computerId() {
   try {
     if (fs.existsSync(filepath)) {
       return fs.readFileSync(filepath, "utf-8");
     }
-    process.stdout.write("File does not exist!");
+    process.stdout.write("File does not exist! Generating a new one! \n");
+    const randomString = generateRandomString(30);
+    fs.writeFileSync(filepath, randomString);
   } catch (e) {
+    console.error(e);
     process.stdout.write("\nRequest failed! Press Control+C to quit \n");
     while (1);
   }
